9cfadb61b0d97d86ff7bae621b2ba3824f8de879
jessicat8 <jessicatong14@gmail.com>
2025-07-18 12:27:50 -0400

    incorporate real PSC-CSC transfer times

--- EMS-data/ems-strokes-traveltimes.py
+++ EMS-data/ems-strokes-traveltimes.py
@@ -3,10 +3,50 @@ import numpy as np
 
 df_CSC = pd.read_csv("closest_comprehensive_hospitals.csv")
 df_PSC = pd.read_csv("closest_primary_hospitals.csv")
+transfer_times = pd.read_csv("hospital_to_hospital_distances.csv")
 
-df_CSC = df_CSC.rename(columns={'travel_time_minutes': 'CSC_travel_time_minutes'})
-df_PSC= df_PSC.rename(columns={'travel_time_minutes': 'PSC_travel_time_minutes'})
+df_CSC = df_CSC.drop(columns="dest_type")
+df_PSC = df_PSC.drop(columns="dest_type")
 
-merged_df = pd.merge(df_CSC, df_PSC, on=['origin_lat', 'origin_lon'], how='inner')
+print(df_CSC.shape)
+print(df_PSC.shape)
 
-print(merged_df.head())
+
+df_CSC = df_CSC.rename(columns={'travel_time_minutes': 'CSC_travel_time_minutes', 'dest_lat':'CSC_dest_lat', 'dest_lon':'CSC_dest_lon','distance_km':'CSC_distance_km','method':'CSC_method'})
+df_PSC = df_PSC.rename(columns={'travel_time_minutes': 'PSC_travel_time_minutes', 'dest_lat':'PSC_dest_lat', 'dest_lon':'PSC_dest_lon','distance_km':'PSC_distance_km','method':'PSC_method'})
+
+df = pd.merge(df_CSC, df_PSC, on=['origin_lat', 'origin_lon'], how='inner')
+print(df.columns)
+print(df.shape)
+
+# merge in transfer times from closest PSC to closest CSC
+
+# Round all lat/lon columns to 4 decimal places to avoid precision mismatch
+df[['PSC_dest_lat', 'PSC_dest_lon', 'CSC_dest_lat', 'CSC_dest_lon']] = df[['PSC_dest_lat', 'PSC_dest_lon', 'CSC_dest_lat', 'CSC_dest_lon']].round(4)
+transfer_times[['primary_lat', 'primary_lon', 'comprehensive_lat', 'comprehensive_lon']] = transfer_times[['primary_lat', 'primary_lon', 'comprehensive_lat', 'comprehensive_lon']].round(4)
+
+
+df = pd.merge(
+    df,
+    transfer_times,
+    left_on=['PSC_dest_lat', 'PSC_dest_lon', 'CSC_dest_lat', 'CSC_dest_lon'],
+    right_on=['primary_lat', 'primary_lon', 'comprehensive_lat', 'comprehensive_lon'],
+    how='left'
+)
+
+df = df.rename(columns={'travel_time_minutes': 'transfer_time_minutes', 'distance_km': 'transfer_distance_km'})
+
+# drop redundant hospital lat long
+df.drop(columns=['primary_lat', 'primary_lon', 'comprehensive_lat', 'comprehensive_lon'], inplace=True)
+# drop from to columns
+df.drop(columns=['From', 'To'], inplace=True)
+
+print(df.head())
+print(df.columns)
+print(df.shape)
+
+
+# Save to CSV
+output_file = 'ems-strokes-traveltimes_with-transfer.csv'
+df.to_csv(output_file, index=False)
+print(f"Merged travel times saved to {output_file}")
--- input_patients.py
+++ input_patients.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 def set_input_patients(sex, age, RACE_score, time_since_LKW, output_file):
-    df = pd.read_csv("EMS-data/ems-strokes-traveltimes.csv")
+    df = pd.read_csv("EMS-data/ems-strokes-traveltimes_with-transfer.csv")
 
     #choosing these values arbitrarily for now
     df['sex'] = sex
     df['age'] = age # majority of strokes occur in people aged 65 and older
     df['RACE'] = RACE_score # cutoff > 5 for LVO, max score of 9, this may be different from mRACE
     df['time_since_symptoms'] = time_since_LKW # 2hrs
-    df['transfer_time'] = abs(df['CSC_travel_time_minutes'] - df['PSC_travel_time_minutes']) #for now taking the diff, definitely not correct
+    df['transfer_time'] = df['transfer_time_minutes']
 
     # Save to CSV
     df.to_csv(output_file, index=False)
